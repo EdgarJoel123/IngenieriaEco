02cf38f645ec6d68552e3bf1456d48e2f9144e4a
Edgar <esanchez1169@uta.edu.ec>
2023-05-25 15:00:53 -0500

    Hacer la automatizacion francesa

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -12,7 +12,19 @@ export class InicioComponent implements OnInit {
   selectedEntidadFinanciera: any;
   selectedValue: string = '';
   selectedData: any;
+ // selectedPorcentaje: number;
 
+  selectedPorcentaje: number
+  anios: number;
+  monto: number;
+  amortizacion: any[] = [];
+
+
+  data = {
+    key: '',
+    value: ''
+  };
+  
   opcionesPrestamo: any = {
     CONSUMO: "CONSUMO",
     MICROCREDITO: "MICROCREDITO",
@@ -28,11 +40,20 @@ export class InicioComponent implements OnInit {
     this.selectedValue = this.selectedEntidadFinanciera.id;
     this.selectedData = this.getSelectedEntidadFinancieraData();
   }
-  
+
+  updateSelectedPorcentaje(value: number) {
+    this.selectedPorcentaje = value;
+  }
+
+
+  seleccionarPorcentaje(porcentaje: number) {
+    this.selectedPorcentaje = porcentaje; // Almacena el porcentaje seleccionado
+  }
+
   getKeys(obj: any): string[] {
     return Object.keys(obj);
   }
-  
+
   obtenerEntidadFinanciera() {
     this.http.get<any[]>('http://localhost:3000/entidadFinanciera').subscribe(data => {
       this.entidadFinancieraData = data;
@@ -40,11 +61,11 @@ export class InicioComponent implements OnInit {
   }
 
   // Actualizar la función onSelectEntidadFinanciera
-onSelectEntidadFinanciera(event: any) {
-  const id = event.target.value;
-  this.selectedEntidadFinanciera = this.entidadFinancieraData.find(entidad => entidad.id === id);
-  this.selectedValue = this.opcionesPrestamo.CONSUMO; // Establecer el valor predeterminado del segundo select
-}
+  onSelectEntidadFinanciera(event: any) {
+    const id = event.target.value;
+    this.selectedEntidadFinanciera = this.entidadFinancieraData.find(entidad => entidad.id === id);
+    this.selectedValue = this.opcionesPrestamo.CONSUMO; // Establecer el valor predeterminado del segundo select
+  }
 
   getSelectedEntidadFinancieraData(): any[] {
     if (this.selectedEntidadFinanciera) {
@@ -54,7 +75,55 @@ onSelectEntidadFinanciera(event: any) {
     }
     return [];
   }
+
+
+  /*calcular() {
+    this.amortizacion = [];
+    //const selectedValueNumber = parseFloat(this.selectedValue.replace(',', '.'));
+    this.calcularAmortizacion(this.selectedPorcentaje, this.anios, this.monto);
+    //console.log(this.selectedValue)
+  }*/
+
+  calcular() {
+  this.amortizacion = [];
+  const valueMatch = this.selectedValue.match(/:\s*([\d.]+)/);
+  if (valueMatch) {
+    const selectedValueNumber = parseFloat(valueMatch[1]);
+    if (!isNaN(selectedValueNumber)) {
+      this.calcularAmortizacion(selectedValueNumber, this.anios, this.monto);
+    } else {
+      console.error("El valor numérico no es válido.");
+    }
+  } else {
+    console.error("El formato no coincide (no se encontró el valor numérico).");
+  }
+}
+
   
   
+  
+  calcularAmortizacion(porcentaje: number, años: number, monto: number) {
+    const meses = años * 12;
+    const tasaMensual = porcentaje / 12 / 100;
+    const cuota = (monto * tasaMensual) / (1 - Math.pow(1 + tasaMensual, -meses));
+
+    let saldo = monto;
+
+    for (let mes = 1; mes <= meses; mes++) {
+      const interes = saldo * tasaMensual;
+      const capital = cuota - interes;
+      saldo -= capital;
+
+      const fila = {
+        mes: mes,
+        cuota: cuota.toFixed(2),
+        interes: interes.toFixed(2),
+        capital: capital.toFixed(2),
+        saldo: saldo.toFixed(2)
+      };
+
+      this.amortizacion.push(fila);
+    }
+  }
   
 }
