da3dddb72f8a729532f369a4b23172745e239aa5
NANA1207 <74752173+NANA1207@users.noreply.github.com>
2023-05-25 04:14:33 -0500

    admin diseño

--- src/app/components/vista-admin/vista-admin.component.ts
+++ src/app/components/vista-admin/vista-admin.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var window:any;
 @Component({
   selector: 'app-vista-admin',
   templateUrl: './vista-admin.component.html',
   styleUrls: ['./vista-admin.component.css']
 })
-export class VistaAdminComponent {
+export class VistaAdminComponent implements OnInit {
 
+  formModal:any;
+  formModalNuevo:any;
+  constructor(){}
+  ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('myModal')
+    );
+    this.formModalNuevo= new window.bootstrap.Modal(
+      document.getElementById('modalNuevo')
+    );
+  }
+  
+  openFormModal() {
+    this.formModal.show();
+  }
+  saveSomeThing() {
+    // confirm or save something
+    this.formModal.hide();
+  }
+
+  openModalNuevo(){
+    this.formModalNuevo.show();
+    this.formModal.hide();
+  }
+
+  closeNuevo(){
+    this.formModalNuevo.hide();
+    this.formModal.show();
+  }
 }
